66acc0fb22cb028cc59105d6f41247c80c8e947a
Theo Dedeken <theo.dedeken@ugent.be>
2017-12-31 12:08:56 +0100

    basic vector math

--- src/include/math/VectorInt.hpp
+++ src/include/math/VectorInt.hpp
@@ -11,6 +11,12 @@ class VectorInt {
         int getSize() const;
         int getValue(int index);
         void setValue(int index, int value);
+        
+        VectorInt* plus(VectorInt* b);
+        VectorInt* minus(VectorInt* b);
+        VectorInt* mult(int a);
+        VectorInt* div(int a);
+        int dot(VectorInt* b);
 };
 
 EMSCRIPTEN_BINDINGS(vector_int) {
@@ -19,5 +25,10 @@ EMSCRIPTEN_BINDINGS(vector_int) {
     .property("size", &VectorInt::getSize)
     .function("getValue", &VectorInt::getValue)
     .function("setValue", &VectorInt::setValue)
+    .function("plus", &VectorInt::plus, allow_raw_pointers())
+    .function("minus", &VectorInt::minus, allow_raw_pointers())
+    .function("mult", &VectorInt::mult, allow_raw_pointers())
+    .function("div", &VectorInt::div, allow_raw_pointers())
+    .function("dot", & VectorInt::dot, allow_raw_pointers())
     ;
 }
--- src/main/math/VectorInt.cpp
+++ src/main/math/VectorInt.cpp
@@ -21,3 +21,61 @@ void VectorInt::setValue(int index, int value){
     _values[index] = value;
 }
 
+VectorInt* VectorInt::plus(VectorInt* b){
+    if (_size == b->getSize()){
+        VectorInt* res = new VectorInt::VectorInt(_size);
+        for (int i = 0; i < _size; i++){
+            res->setValue(i, _values[i] + b->getValue(i));
+        }
+        return res;
+    }
+    else {
+        //TODO error
+        return nullptr;
+    }
+}
+
+VectorInt* VectorInt::minus(VectorInt* b){
+    if (_size == b->getSize()){
+        VectorInt* res = new VectorInt::VectorInt(_size);
+        for (int i = 0; i < _size; i++){
+            res->setValue(i, _values[i] - b->getValue(i));
+        }
+        return res;
+    }
+    else {
+        //TODO error
+        return nullptr;
+    }
+}
+
+VectorInt* VectorInt::mult(int a){
+    VectorInt* res = new VectorInt::VectorInt(_size);
+    for (int i = 0; i < _size; i++){
+        res->setValue(i, _values[i]*a);
+    }
+    return res;
+}
+
+VectorInt* VectorInt::div(int a){
+    VectorInt* res = new VectorInt::VectorInt(_size);
+    for (int i = 0; i < _size; i++){
+        res->setValue(i, _values[i]/a);
+    }
+    return res;
+}
+
+int VectorInt::dot(VectorInt* b){
+    if (_size == b->getSize()){
+        int res = 0;
+        for (int i = 0; i < _size; i++){
+            res += _values[i] * b->getValue(i);
+        }
+        return res;
+    }
+    else {
+        //TODO error
+        return -1;
+    }
+}
+
